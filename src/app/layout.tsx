357cb4b8f0dadc90d3ac8846d020835cc6219f33
Rishith Kumar Guntuka <spargy4050v@gmail.com>
2025-10-18 14:27:13 +0530

    refactor: simplify RootLayout by removing unnecessary HTML structure and imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,12 @@
 import type { Metadata } from 'next'
 
-import { cn } from '@/utilities/ui'
-import { GeistMono } from 'geist/font/mono'
-import { GeistSans } from 'geist/font/sans'
-import React from 'react'
-
-import { Providers } from '@/providers'
-import { InitTheme } from '@/providers/Theme/InitTheme'
 import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
-
-import './(frontend)/globals.css'
 import { getServerSideURL } from '@/utilities/getURL'
 
+import './(frontend)/globals.css'
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
-      <head>
-        <InitTheme />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
-      </head>
-      <body>
-        <Providers>
-          {children}
-        </Providers>
-      </body>
-    </html>
-  )
+  return children
 }
 
 export const metadata: Metadata = {
