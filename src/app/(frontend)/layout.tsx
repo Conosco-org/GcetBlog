420698c6dedd0af3cf8389fee756273a75d250f3
Rishith Kumar Guntuka <spargy4050v@gmail.com>
2025-10-18 14:27:31 +0530

    refactor: restructure FrontendLayout to include proper HTML structure and improve readability

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
+import { cn } from '@/utilities/ui'
+import { GeistMono } from 'geist/font/mono'
+import { GeistSans } from 'geist/font/sans'
 
 import { Footer } from '@/Footer/Component'
 import { Header } from '@/Header/Component'
+import { Providers } from '@/providers'
+import { InitTheme } from '@/providers/Theme/InitTheme'
 
 export default async function FrontendLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
+    <html className={cn(GeistSans.variable, GeistMono.variable)} lang="en" suppressHydrationWarning>
+      <head>
+        <InitTheme />
+        <link href="/favicon.ico" rel="icon" sizes="32x32" />
+        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+      </head>
+      <body>
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
+    </html>
   )
 }
